84e1468165873e2baabf3d4fc38e5592bf73c254
Guilherme Agostini <gui.agostini@hotmail.com>
2022-07-24 22:04:39 -0300

    Adicionado o projeto alura-stickers

---
+++ src/ImDbAPI.java
@@ -0,0 +1,31 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ImDbAPI{
+
+public String Top250Movies() throws IOException{
+    Properties key = getKey(); // Atribui o conteúdo
+    return key.getProperty("prop.urlTop250Movies") + key.getProperty("prop.keyTop250Movies");
+}
+
+public String MostPopularMovies() throws IOException{
+    Properties key = getKey(); // Atribui o conteúdo
+    return key.getProperty("prop.urlMostPopularMovies") + key.getProperty("prop.keyMostPopularMovies");
+}
+
+public String MostPopularTVs() throws IOException{
+    Properties key = getKey(); // Atribui o conteúdo
+    String link = key.getProperty("prop.urlMostPopularTVs") + key.getProperty("prop.keyMostPopularTVs");
+    return link;
+}
+
+public Properties getKey() throws IOException {
+    Properties key = new Properties(); // Instancia a classe Properties
+    FileInputStream prop = new FileInputStream("./key.properties"); // Busca o arquivo
+    key.load(prop); // Carrega o conteúdo
+    return key;
+}
+
+
+}
---
+++ src/Movies.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.Map;
+
+public class Movies {
+	private List<Map<String, String>> items;
+
+	public List<Map<String, String>> getItems() {
+		return items;
+	}
+}
