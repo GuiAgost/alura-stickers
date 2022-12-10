faa76548173811427be89adeda26b0b23e55a95b
Guilherme <gui.agostini@hotmail.com>
2022-12-09 21:50:07 -0300

    Configurar a fonte e obter a imagem

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -11,6 +13,7 @@ public class App {
             int opcao;
             String url;
 
+            // MENU OPÇÃO
             System.out.println("*=========Escolha sua imDbAPI=========");
             System.out.println("1 - Top250Movies");
             System.out.println("2 - MostPopularMovies");
@@ -30,28 +33,36 @@ public class App {
             List<Map<String, String>> listFilms = ExtractorImDbAPI.getFilms(url);
 
             // EXIBIR E MANIPULAR OS DADOS
+            var geradora = new GeradoraDeFigurinhas();
             for (Map<String, String> films : listFilms) {
 
-                    System.out.println("\u001b[32;1m\u001b[4mTítulo:\u001b[m " + "\u001b[36;1m" +films.get("title") +"\u001b[m");
-                    System.out.println("\u001b[34;1m\u001b[4mImagem:\u001b[m " + films.get("image"));
+                String urlImagem = films.get("image");
+                String titulo = films.get("title");
+                String classificacao = films.get("imDbRating");
 
-                    float numberStar;
-                    // SE A NOTA FOR VAZIA
-                    if( films.get("imDbRating").isEmpty()){ 
-                        System.out.println("\u001b[30m\u001b[1m\u001b[4mClassificação Geral: 0.0" + " \u001b[m\u001B[33m\u001b[m");
-                        System.out.print(TERRIBLE);
-                    } else {
-                    System.out.println("\u001b[30m\u001b[1m\u001b[4mClassificação Geral: " + films.get("imDbRating") + " \u001b[m\u001B[33m\u001b[m");
+                // PARA GERAR A IMAGEM FIGURINHA
+                InputStream inputStream = new URL(urlImagem).openStream(); // A PARTIR DA URL DA IMAGEM
+                geradora.cria(inputStream, titulo + ".png");
 
-                    numberStar = (float) Double.parseDouble(films.get("imDbRating"));
-                    for (int i = 1; i <= Math.abs(numberStar); i++){
+                // EXIBE O TITULO, URL DA IMAGEM E A CLASSIFICAÇÃO
+                System.out.println("\u001b[32;1m\u001b[4mTítulo:\u001b[m " + "\u001b[36;1m" + titulo + "\u001b[m");
+                //System.out.println("\u001b[34;1m\u001b[4mImagem:\u001b[m " + urlImagem);
+
+                float numberStar;
+                // SE A NOTA FOR VAZIA
+                if (films.get("imDbRating").isEmpty()) {
+                    System.out.println("\u001b[30m\u001b[1m\u001b[4mClassificação Geral: 0.0" + " \u001b[m\u001B[33m\u001b[m");
+                    System.out.print(TERRIBLE);
+                } else {
+                    System.out.println("\u001b[30m\u001b[1m\u001b[4mClassificação Geral: " + classificacao + " \u001b[m\u001B[33m\u001b[m");
+
+                    numberStar = (float) Double.parseDouble(classificacao);
+                    for (int i = 1; i <= Math.abs(numberStar); i++) {
                         System.out.print(STAR_YELOW);
                     }
                 }
-                
-
                 System.out.println("\n\n======================================================\n");
             }
-        }        
+        }
     }
 }
--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,15 +1,20 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
+import java.awt.Color;
+import java.awt.Font;
 
 import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
 
-    public void cria() throws Exception{
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception{
 
     // LEITURA DA IMAGEM
-    BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+    //InputStream inputStream = new FileInputStream(new File("entrada/filme-maior.jpg")); // NESSE CASO, CRIAR A PASTA "ENTRADA" QUE CONTENHA IMAGEM
+    //InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg").openStream(); // A PARTIR DA URL DA IMAGEM
+    BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
     // CRIA UMA NOVA IMAGEM COM MEMÓRIA COM TRANSPARÊNCIA E TAMANHO NOVO
     int largura = imagemOriginal.getWidth();
@@ -21,17 +26,24 @@ public class GeradoraDeFigurinhas {
     Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
     graphics.drawImage(imagemOriginal, 0, 0, null);
 
+    // CONFIGURAR FONTE
+    Font font = new Font(Font.SANS_SERIF, Font.BOLD, 60);
+    graphics.setColor(Color.GREEN);
+    graphics.setFont(font);
+
     // ESCREVER UMA FRASE NA NOVA IMAGEM
-    // PAREI NO 00:30:48!!!!!
+    graphics.drawString("TOPZERA", 200, novaAltura - 80);
 
     // ESCREVER A NOVA IMAGEM EM UM ARQUIVO
-    ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
+    ImageIO.write(novaImagem, "png", new File(nomeArquivo));
 
 
     }
 
-    public static void main(String[] args) throws Exception {
-        GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
-        geradora.cria();
-    }
+    // public static void main(String[] args) throws Exception {
+    //     GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
+    //     geradora.cria();
+    // }
 }
+
+// VERIFICAR O PROBLEMA: https://stackoverflow.com/questions/2408613/unable-to-read-jpeg-image-using-imageio-readfile-file/16149142#16149142
