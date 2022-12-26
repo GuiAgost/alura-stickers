44c06b6689e93084f9d7febe1ba3eef31feaf92f
Guilherme Agostini <gui.agostini@hotmail.com>
2022-12-26 20:29:47 -0300

    Refatorado a classe GeradoraDeFigurinhas.

--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
-
 import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
@@ -20,26 +20,33 @@ public class GeradoraDeFigurinhas {
         int novaAltura = altura + 70;
         BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
-        // COPIAR A IMAGEM ORIGINAL PRA NOVA IMAGEM (EM MEMÓRIA)
+        newImage(imagemOriginal, novaAltura, novaImagem);
+        directory(nomeArquivo, novaImagem);
+    }
+    // COPIAR A IMAGEM ORIGINAL PRA NOVA IMAGEM (EM MEMÓRIA)
+    private static void newImage(BufferedImage imagemOriginal, int novaAltura, BufferedImage novaImagem) throws FontFormatException, IOException {
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
+        configureFont(novaAltura, graphics);
+    }
 
-        // CONFIGURAR FONTE
+    // CONFIGURA A FONTE E ESCREVE UMA PAAVRA NA NOVA IMAGEM
+    private static void configureFont(int novaAltura, Graphics2D graphics) throws FontFormatException, IOException {
         //Font font = new Font(Font.SANS_SERIF, Font.BOLD, 14);
         Font font = Font.createFont(Font.ROMAN_BASELINE, new File("fonts/ComicNeueSansID.ttf"))
                 .deriveFont(Font.PLAIN, 14);
         graphics.setColor(Color.GREEN);
         graphics.setFont(font);
 
-        // ESCREVER UMA FRASE NA NOVA IMAGEM
         graphics.drawString("TOPZERA", 35, novaAltura - 50);
+    }
 
-        // ESCREVER A NOVA IMAGEM EM UM ARQUIVO
+    // CRIA NOVO DIRETÓRIO E SALVA A IMAGEM
+    private static void directory(String nomeArquivo, BufferedImage novaImagem) throws IOException {
         File directory = new File("saida/");
         if (directory.exists() || directory.mkdir()){
             ImageIO.write(novaImagem, "png", new File(nomeArquivo));
         }
     }
 }
-
 // https://unicode.org/emoji/charts/full-emoji-list.html
