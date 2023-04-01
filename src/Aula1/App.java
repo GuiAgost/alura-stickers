ddfcad93a9e513ebe8ed3525d17f280be5df3f9b
Guilherme Agostini <gui.agostini@hotmail.com>
2023-03-31 21:54:10 -0300

    Adicionado Aula 1.

---
+++ src/Aula1/App.java
@@ -0,0 +1,80 @@
+package Aula1;
+
+import com.google.gson.Gson;
+import java.io.FileInputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class App {
+    private static final String STAR_YELOW = "\u001b[m\u001B[33m★\u001b[m";
+    private static final String TERRIBLE = "\u001b[m\u001B[33m\uD83D\uDC4E";
+
+    public static void main(String[] args) throws Exception {
+        try (Scanner input = new Scanner(System.in)) {
+            int opcao;
+            String url;
+
+            // MENU
+            System.out.println("*=========Escolha sua imDbAPI=========");
+            System.out.println("1 - Top250Movies");
+            System.out.println("2 - MostPopularMovies");
+            System.out.println("3 - MostPopularTVs");
+            opcao = input.nextInt();
+
+            // FAZER UMA CONEXÃO HTTP E BUSCAR OS TOP 250 FILMES
+            Properties key = new Properties();
+            FileInputStream prop = new FileInputStream("./key.properties"); // Busca o arquivo
+            key.load(prop); // Carrega o conteúdo
+
+            // PEGA O CONTEÚDO CHAVE API
+            url = switch (opcao) {
+                case 1 -> key.getProperty("prop.urlTop250Movies") + key.getProperty("prop.keyTop250Movies");
+                case 2 -> key.getProperty("prop.urlMostPopularMovies") + key.getProperty("prop.keyMostPopularMovies");
+                case 3 -> key.getProperty("prop.urlMostPopularTVs") + key.getProperty("prop.keyMostPopularTVs");
+                default -> throw new IllegalStateException("Unexpected value: " + opcao);
+            };
+
+            URI endereco = URI.create(url);
+            var client = HttpClient.newHttpClient();
+            var request = HttpRequest.newBuilder(endereco).GET().build();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            String body = response.body();
+
+            // EXTRAIR SÓ OS DADOS QUE INTERESSAM (TITULO, POSTER E CLASSIFICAÇÃO)
+            Gson gson = new Gson();
+            Movies movieList = gson.fromJson(body, Movies.class);
+            List<Map<String, String>> listaDeFilmes = movieList.getItems();
+
+            // EXIBIR E MANIPULAR OS DADOS
+            for (Map<String, String> filme : listaDeFilmes) {
+                String titulo = filme.get("title");
+                String classificacao = filme.get("imDbRating");
+                String linkImagem = filme.get("image");
+
+                System.out.println("\u001b[32;1m\u001b[4mTítulo:\u001b[m " + "\u001b[36;1m" + titulo + "\u001b[m");
+                System.out.println("\u001b[37;1m\u001b[44mLink de imagem:\u001b[m " + linkImagem);
+
+                // SE A CLASSIFICAÇÃO VAZIA, RETORNA 0.0F
+                float numberStar = Float.parseFloat(classificacao.isEmpty() ? String.valueOf(0.0F) : classificacao);
+                // SE A NOTA FOR VAZIA
+                if (classificacao.isEmpty()) {
+                    System.out.println("\u001b[34m\u001b[1m\u001b[4mClassificação Geral: 0.0" + " \u001b[m\u001B[33m\u001b[m");
+                    System.out.println(TERRIBLE);
+                } else {
+                    System.out.println("\u001b[34m\u001b[1m\u001b[4mClassificação Geral: " + classificacao + " \u001b[m\u001B[33m\u001b[m");
+                    for (int i = 1; i <= Math.abs(numberStar); i++) {
+                        System.out.print(STAR_YELOW);
+                    }
+                }
+                System.out.println("\n\n======================================================\n");
+            }
+        }
+    }
+}
---
+++ src/Aula1/Movies.java
@@ -0,0 +1,13 @@
+package Aula1;
+
+import java.util.List;
+import java.util.Map;
+
+public class Movies {
+
+	private List<Map<String, String>> items;
+
+	public List<Map<String, String>> getItems() {
+		return items;
+	}
+}
